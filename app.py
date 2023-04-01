3b4492c0aa3f9a9b3c91ac45be20cb93f70d86a8
Amartya <amar182k2@gmail.com>
2023-04-01 13:59:57 +0530

    get_reports-route

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 
 import routes.user as user
 import routes.inference as inference
+import routes.reports as reports
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app.add_url_rule('/register_user', methods=["POST"], view_func=user.register_use
 app.add_url_rule('/login', methods=["POST"], view_func=user.login)
 
 app.add_url_rule('/run_inference', methods=["POST"], view_func=inference.run_inference)
+app.add_url_rule('/get_reports', methods=['POST'], view_func=reports.get_reports())
 client = MongoClient('localhost', 27017)
 
 db = client.kavach
